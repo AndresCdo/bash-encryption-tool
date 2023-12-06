6f8526714698dad84cb4a8f0959ea1906a36fd0a
Andres Caicedo <andres.felipe.caicedo.ultengo@outlook.com>
2023-12-06 21:48:23 +0100

    Update bash_encryption_tool.sh
    Add dependence

--- bash_encryption_tool.sh
+++ bash_encryption_tool.sh
@@ -7,6 +7,17 @@ algorithm="aes-256-cbc"
 # Define iteration count
 iter=10000
 
+# Check if xxd is installed
+if command -v xxd &> /dev/null; then
+    echo "xxd is already installed."
+else
+    # Install xxd
+    echo "xxd is not installed. Installing..."
+    sudo apt-get update
+    sudo apt-get install vim
+    echo "Installation complete."
+fi
+
 if [[ $# -ne 2 ]]; then
     echo "Usage: $0 [-e|-d] folder_name" >&2
     exit 1
